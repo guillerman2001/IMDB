e974253d562bdb3d3b3b47ba3db4e457ee9b563d
Natalia <nathaperez1718@gmail.com>
2021-10-15 13:07:43 +0200

    añadido main

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { Professional } from "./imbd";
+
+
+let professional : Professional = new Professional("Oscar", 22, 90, 180, "White", "Brown", "One", true, "España", 2, "Actor")
+let professional1 : Professional = new Professional("Nate", 29, 80, 171, "Brown", "Blue", "Two", false, "England", 4, "eSports Player")
+
+professional.print()
+professional1.print()
