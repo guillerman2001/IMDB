7beff944b17a1975974240effaf9f63bb3039036
guillerman2001 <78086935+guillerman2001@users.noreply.github.com>
2021-10-15 15:41:59 +0200

    Movie

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+import { Imdb } from './imdbClass';
+import { Movie } from "./movie";
 import { Professional } from "./imbd";
 
 
@@ -6,3 +8,24 @@ let professional1 : Professional = new Professional("Nate", 29, 80, 171, "Brown"
 
 professional.print()
 professional1.print()
+
+let pelicula1 = new Movie('Capitana Marvel',2020,'Estados Unidos','accion');
+
+pelicula1.actors = ['Guillermo Ramos','Fernando Diaz','Carmen Fernandez'];
+pelicula1.director = 'Guillermo Ramos';
+pelicula1.writer = 'Eduardo Ramos';
+pelicula1.language = 'ingles';
+pelicula1.plataform = 'cine';
+pelicula1.isMCU = true;
+pelicula1.mainCharacterName = 'Jesica Jones';
+pelicula1.producer = 'Tomas Castillo';
+pelicula1.distributor = 'Walt Disney';
+
+pelicula1.printAll();
+
+let pelicula2 = new Movie('Capitan America',2015,'Estados Unidos','accion');
+let pelicula3 = new Movie('Torrente5',2015,'España','comedia');
+let pelicula4 = new Movie('Me quiero comer tu pancreas',2017,'Japon','romantica');
+
+let imdb1 = new Imdb([pelicula2,pelicula3,pelicula4])
+console.log(imdb1);
